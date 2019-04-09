d96b92c05cc57f4efd03c70a0d414d96bc9b9be4
hassenbm88 <47831160+hassenbm88@users.noreply.github.com>
2019-04-09 22:29:00 +0100

    Add files via upload

---
+++ fonction.h
@@ -0,0 +1,10 @@
+#ifndef FONCTION_H_
+#define FONCTION_H_
+#include<stdio.h>
+#include<stdlib.h>
+#include<SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+void pause();
+int gestion_score(int collision,int i);
+#endif
--- main.c
+++ main.c
@@ -1,39 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-#include<time.h>
-#include"SDL/SDL.h"
-#include"SDL/SDL_image.h"
-#include"SDL/SDL_mixer.h"
-#include"game.h"
+#include<SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include"fonction.h"
 
-
-int  main()
-{
-
-SDL_Surface*screen;
-SDL_Rect positionecran;
-SDL_Event event;
-
-
-
-enemy E;
-
-
-    SDL_Init(SDL_INIT_VIDEO);
-    
-
-    screen = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-    SDL_WM_SetCaption("Afficher Ennemie en Mvt Aleatoire Va et Vient sur x", NULL);
- initialiser_enemy(&E);
-   
-
-deplacement_aleatoire(E,screen);
-
-  SDL_FreeSurface(E.image_enemy);
-    SDL_Quit();
-
-
-
-
+int main(){
+int collision=1;
+int i=3;
+i=gestion_score(collision,i);
 }
+
+

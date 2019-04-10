8034abe79f41b0eb2308b3628f45ae5e00af5fdd
mulgan25ngougbia <47834871+mulgan25ngougbia@users.noreply.github.com>
2019-04-10 19:40:07 +0100

    Update and rename Initialiser_Afficher_background to Initialiser_Afficher_background.c

--- Initialiser_Afficher_background.c
+++ Initialiser_Afficher_background.c
@@ -9,29 +9,6 @@ Computer World 2D
 #include <string.h>
 #include <math.h>
 
-#ifndef FILE_PROS_H_INCLUDED
-#define FILE_PROS_H_INCLUDED
-typedef struct 
-{
-    int stage;//numero de stage
-    SDL_Rect pos;
-    SDL_Rect post_background;//position du background par rapport a l ecran
-    SDL_Surface *image[1];//tableau de background
-   /* SDL_Surface* bg_collision;*/
-    SDL_Rect position_collision;
-    SDL_Rect scroll;
-    
-}background;
-
-//Prototype des fonctions
-
-void initialiser_background(background *Back);
-void afficher_background(SDL_Surface *ecran, background Back);
-void scrolling_background(background *Back, int etat);//scroller verticalement et /ou horizontalement tout le cadre si le perso atteint les limites du cadre
-#endif // FILE_PROS_H_INCLUDED
-
-
-
 void initialiser_background(background *Back)
 {
     Back->stage = 0;

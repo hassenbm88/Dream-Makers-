acc82eb86bfacedf36bdb14a3a5c905664b503a4
mulgan25ngougbia <47834871+mulgan25ngougbia@users.noreply.github.com>
2019-04-10 20:12:46 +0100

    Update Getpixel.h

--- Getpixel.h
+++ Getpixel.h
@@ -1,8 +1,15 @@
 # Dream-Makers-
 Computer World 2D 
-#ifndef FILE_PROS_H_INCLUDED
-#define FILE_PROS_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
 
 SDL_Color GetPixel (SDL_Surface* pSurface , int x , int y);
 
-#endif // FILE_PROS_H_INCLUDED
+

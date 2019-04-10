a904fa4d505e9e4a70f1cf7467afb14bad5b35db
mulgan25ngougbia <47834871+mulgan25ngougbia@users.noreply.github.com>
2019-04-10 20:05:06 +0100

    Update Collision_personnage_background.h

--- Collision_personnage_background.h
+++ Collision_personnage_background.h
@@ -3,6 +3,6 @@ Computer World 2D
 #ifndef FILE_PROS_H_INCLUDED
 #define FILE_PROS_H_INCLUDED
 
-
+bool collisionmap (int direction,SDL_Rect col,SDL_Surface *collisionMap,SDL_Surface *ecran);
 
 #endif // FILE_PROS_H_INCLUDED

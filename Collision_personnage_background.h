b870f366406061dda6f0fd8c0feccd58d37924ba
mulgan25ngougbia <47834871+mulgan25ngougbia@users.noreply.github.com>
2019-04-10 20:09:24 +0100

    Update Collision_personnage_background.h

--- Collision_personnage_background.h
+++ Collision_personnage_background.h
@@ -1,9 +1,16 @@
 # Dream-Makers-
 Computer World 2D 
-#ifndef FILE_PROS_H_INCLUDED
-#define FILE_PROS_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <SDL/SDL.h
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
 
 bool collisionmap (int direction,SDL_Rect col,SDL_Surface *collisionMap,SDL_Surface *ecran);
-
 bool collision(SDL_Rect* coll, SDL_Rect* coll1);
-#endif // FILE_PROS_H_INCLUDED
+
